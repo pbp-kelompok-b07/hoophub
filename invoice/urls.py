a5f48878ef2bd0760fd54e280a1e59a2737c9105
akhtareijaz <akhtareijaz@gmail.com>
2025-10-22 15:44:20 +0700

    menambahkan model untuk invoice

--- invoice/urls.py
+++ invoice/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-# from invoice.views import 
+from invoice.views import show_invoices, invoice_detail
 
 app_name = 'invoice'
 
 urlpatterns = [
-
+    path("", show_invoices, name="show_invoices"),
+    path("invoice", invoice_detail, name="invoice_detail"),
 ]
--- invoice/views.py
+++ invoice/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import datetime
+from django.http import HttpResponseRedirect, JsonResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
+from invoice.models import Invoice
 
 # Create your views here.
+
+@login_required(login_url='/authenticate/login')
+def show_invoices(request):
+    invoice = Invoice.objects.filter(user=request.user)
+    return render(request, "invoices.html", invoice)
+
+@login_required(login_url='/authenticate/login')
+def invoice_detail(request, id):
+    invoice = get_object_or_404(Invoice, pk=id)
+    return render(request, "invoice_detail.html", invoice)
