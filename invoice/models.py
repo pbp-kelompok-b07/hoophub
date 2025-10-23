378e9acf9cd3b9fe83299a91e325f83ffe79b7a6
akhtareijaz <akhtareijaz@gmail.com>
2025-10-23 23:04:08 +0700

    pull dengan feature lain

--- invoice/models.py
+++ invoice/models.py
@@ -1,7 +1,8 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import User #, Product
+from django.contrib.auth.models import User
 from django.utils import timezone
+from catalog.models import Product
 
 
 # Create your models here.
@@ -9,6 +10,6 @@ from django.utils import timezone
 class Invoice(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    # product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
     date = models.DateField(default=timezone.now)
     invoice_no = models.CharField(max_length=32, unique=True, db_index=True)
