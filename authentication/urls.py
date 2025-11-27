499081813e28cb8cdc9252bcef7d2a8607b6675a
roseliaevanny <roseliasucipto8@gmail.com>
2025-11-27 20:45:06 +0700

    login register logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from authentication.views import register, login_user, logout_user
+from authentication.views import register, login_user, logout_user, register_flutter, login_flutter, logout_flutter
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout')
+    path('logout/', logout_user, name='logout'),
+    path('register-flutter/', register_flutter, name='register_flutter'),
+    path('login-flutter/', login_flutter, name='login_flutter'),
+    path('logout-flutter/', logout_flutter, name='logout_flutter')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,9 @@ from django.contrib import messages
 from django.http import JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+import json
 
 # Create your views here.
 def register(request):
@@ -43,3 +46,84 @@ def logout_user(request):
     logout(request)
     response = JsonResponse({'success': True, 'message': 'Logged out successfully!'})
     return response
+
+@csrf_exempt
+def login_flutter(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # Login status successful.
+            return JsonResponse({
+                "username": user.username,
+                "status": True,
+                "message": "Login successful!"
+                # Add other data if you want to send data to Flutter.
+            }, status=200)
+        else:
+            return JsonResponse({
+                "status": False,
+                "message": "Login failed, account is disabled."
+            }, status=401)
+
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "Login failed, please check your username or password."
+        }, status=401)
+    
+@csrf_exempt
+def register_flutter(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data['username']
+        password1 = data['password1']
+        password2 = data['password2']
+
+        # Check if the passwords match
+        if password1 != password2:
+            return JsonResponse({
+                "status": False,
+                "message": "Passwords do not match."
+            }, status=400)
+        
+        # Check if the username is already taken
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({
+                "status": False,
+                "message": "Username already exists."
+            }, status=400)
+        
+        # Create the new user
+        user = User.objects.create_user(username=username, password=password1)
+        user.save()
+        
+        return JsonResponse({
+            "username": user.username,
+            "status": 'success',
+            "message": "User created successfully!"
+        }, status=200)
+    
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "Invalid request method."
+        }, status=400)
+    
+@csrf_exempt
+def logout_flutter(request):
+    username = request.user.username
+    try:
+        logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logged out successfully!"
+        }, status=200)
+    except:
+        return JsonResponse({
+            "status": False,
+            "message": "Logout failed."
+        }, status=401)
