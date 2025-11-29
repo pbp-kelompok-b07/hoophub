d9d63c1596c610c9001dad93076b4c048b52d7d9
roseliaevanny <roseliasucipto8@gmail.com>
2025-11-29 18:17:05 +0700

    menambahkan views untuk modul review

--- review/urls.py
+++ review/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from review.views import show_review, show_json, show_json_by_id, create_review, edit_review, delete_review, proxy_image
+from review.views import show_review, show_json, show_json_by_id, create_review, edit_review, delete_review, proxy_image, show_json_flutter, create_review_flutter, edit_review_flutter, delete_review_flutter
 
 app_name = 'review'
 
@@ -11,4 +11,8 @@ urlpatterns = [
     path('edit/<uuid:review_id>/', edit_review, name='edit_review'),
     path('delete/<uuid:review_id>/', delete_review, name='delete_review'),
     path('proxy-image/', proxy_image, name='proxy_image'),
+    path('json-flutter/', show_json_flutter, name='show_json_flutter'),
+    path('create-flutter/<int:id>/', create_review_flutter, name='create_review_flutter'),
+    path('edit-flutter/<uuid:review_id>/', edit_review_flutter, name='edit_review_flutter'),
+    path('delete-flutter/<uuid:review_id>/', delete_review_flutter, name='delete_review_flutter')
 ]
